f8f354a8462c3dcc837616ef9e49854b8f7899b5
danranvm <danranvm@gmail.com>
2020-12-12 18:55:27 +0800

    feat(cdk:clipboard): support useClipboard
    fix #14

---
+++ packages/cdk/clipboard/src/clipboard.ts
@@ -0,0 +1,27 @@
+import { PendingCopy } from './pendingCopy'
+
+export class Clipboard {
+  public static getInstance(): Clipboard {
+    if (!Clipboard.instance) {
+      Clipboard.instance = new Clipboard()
+    }
+    return Clipboard.instance
+  }
+
+  private static instance: Clipboard | null = null
+  private constructor() {
+    // private
+  }
+
+  copy(text: string): boolean {
+    const pendingCopy = this.beginCopy(text)
+    const successful = pendingCopy.copy()
+    pendingCopy.destroy()
+
+    return successful
+  }
+
+  beginCopy(text: string): PendingCopy {
+    return new PendingCopy(text)
+  }
+}
