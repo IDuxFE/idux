39ebe76fc7e94f283fba5dc8787944ca14eb5da4
danranvm <danranvm@gmail.com>
2020-12-20 12:48:10 +0800

    feat: site support standalong demo

---
+++ packages/components/icon/demo/Basic.vue
@@ -0,0 +1,31 @@
+<template>
+  <ix-icon name="up" />
+  <ix-icon name="down" rotate />
+  <ix-icon name="left" rotate="90" />
+  <ix-icon name="right" rotate />
+  <ix-icon name="ix-icon-up" iconfont />
+  <ix-icon name="ix-icon-down" iconfont />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { addIconDefinitions, fetchFromIconfont, useGlobalConfig } from '@idux/components'
+import { Up, Down } from '@idux/components/icon/definitions'
+
+export default defineComponent({
+  setup() {
+    // 静态加载：无需发请求,但是会增加包体积, 推荐首屏加载的图标使用静态加载，减少 http 请求数量
+    addIconDefinitions([Up, Down])
+
+    // 动态加载：不会被打包，可以减小包体积，需要加载的时候时候 http 请求加载
+    const loadIconDynamically = (iconName: string) => {
+      // TODO： fix with vite
+      return fetch(`packages/components/icon/svg/${iconName}.svg`).then(res => res.text())
+    }
+    useGlobalConfig('icon', { loadIconDynamically })
+
+    // 直接引入 iconfont script, 使用时需要注明 iconfont
+    fetchFromIconfont('https://at.alicdn.com/t/font_2269256_s10i6xhg8l.js')
+  },
+})
+</script>
