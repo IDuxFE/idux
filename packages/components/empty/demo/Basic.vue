a3f98a4c6e7a87d0971f4ea678d5490864577745
殷伟鸿 <>
2020-12-31 14:37:37 +0800

    feat(comp:empty): 空数据组件

--- packages/components/empty/demo/Basic.vue
+++ packages/components/empty/demo/Basic.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="empty-demo">
-    <div class="empty-demo-1"><ix-empty></ix-empty></div>
+    <div class="empty-demo-item"><ix-empty /></div>
+    <div class="empty-demo-item">
+      <ix-empty description="抱歉，数据为空" />
+    </div>
+    <div class="empty-demo-item">
+      <ix-empty>
+        <img
+          class="img"
+          width="120"
+          src="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+        />
+        <p class="text">No Data!</p>
+      </ix-empty>
+    </div>
+    <div class="empty-demo-item">
+      <ix-empty
+        image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+      />
+    </div>
   </div>
 </template>
 
@@ -9,10 +27,15 @@
   & > * {
     margin-right: 20px;
   }
-  &-1 {
-    background: #ccc;
+  &-item {
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    background: #fff;
     height: 200px;
     width: 200px;
+    .text {
+      text-align: center;
+    }
   }
 }
 </style>
--- packages/components/empty/src/Empty.vue
+++ packages/components/empty/src/Empty.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="ix-empty-wrapper" :style="imageStyle">
-    <span>
-      <ix-icon name="empty" /><br />
-
-      <p>没有数据</p>
+  <div class="ix-empty-wrapper">
+    <span v-if="slots.default" class="ix-empty-main">
+      <slot />
+    </span>
+    <span v-else class="ix-empty-main">
+      <img v-if="image" :src="image" class="ix-empty-img" :style="imageStyle" />
+      <ix-icon v-else name="empty" class="ix-empty-icon" />
+      <p class="ix-empty-p">{{ description }}</p>
     </span>
   </div>
 </template>
 <script lang="ts">
 import { IxIcon } from '@idux/components/icon'
+import { PropTypes } from '@idux/cdk/utils'
+
 import { defineComponent, PropType, CSSProperties } from 'vue'
 import { EmptyProps } from './types'
 export default defineComponent({
   name: 'IxEmpty',
   components: { IxIcon },
   props: {
-    // description: String,
+    description: PropTypes.string.def('没有数据'),
     imageStyle: { type: Object as PropType<CSSProperties>, default: undefined },
-    // image: String,
+    image: PropTypes.string,
   },
-  setup(props: EmptyProps) {
-    return { props }
+  setup(props: EmptyProps, { slots }) {
+    return { props, slots }
   },
 })
 </script>
