95ec53b6b083881e48e41dc813fb90b08d2f3ba3
liuzaijiang <530604689@qq.com>
2021-01-04 18:42:47 +0800

    docs: update spin component demo

--- packages/components/spin/demo/Icon.vue
+++ packages/components/spin/demo/Icon.vue
@@ -3,9 +3,6 @@
     <ix-spin :icon="icon">
       <div class="content">content</div>
     </ix-spin>
-    <div class="operation-area">
-      <span class="status-text">icon: {{ icon }}</span>
-    </div>
   </div>
 </template>
 
@@ -27,13 +24,5 @@ export default defineComponent({
   text-align: center;
   padding: 30px;
   background: #6e8ef1;
-}
-
-.operation-area {
-  margin-top: 30px;
-  .status-text {
-    margin: 10px 0;
-    display: block;
-  }
 }
 </style>
