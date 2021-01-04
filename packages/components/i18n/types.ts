d6b0fc64c0f8c42c9822c84a0764bc2bb74982d6
lcx15831 <15831@sangfor.com>
2021-01-04 20:25:39 +0800

    refactor(comp:i18n): update useI18n to useLocale

--- packages/components/i18n/types.ts
+++ packages/components/i18n/types.ts
@@ -2,11 +2,16 @@ export interface GlobalLocale {
   placeholder: string
 }
 
+export interface EmptyLocale {
+  description: string
+}
+
 export interface Locale {
   type: LocaleType
   global: GlobalLocale
+  empty: EmptyLocale
 }
 
 export type LocaleKey = keyof Locale
 
-export type LocaleType = 'en_US' | 'es_ES' | 'zh-CN' | 'zh-TW'
+export type LocaleType = 'zh-CN' | 'en-US'
