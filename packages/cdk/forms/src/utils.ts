96de069eb024abe3b422361d75504fc5c0a4cc16
danranVm <danranvm@gmail.com>
2021-01-16 18:15:40 +0800

    feat(cdk:forms): add utils
    re #115

---
+++ packages/cdk/forms/src/utils.ts
@@ -0,0 +1,15 @@
+import type { InjectionKey } from 'vue'
+import type { AbstractControl } from './controls/abstractControl'
+
+import { inject, provide } from 'vue'
+
+const token: InjectionKey<AbstractControl> = Symbol()
+
+export function provideControl(control: AbstractControl): void {
+  provide(token, control)
+}
+
+export function injectControl(path: Array<string | number> | string): AbstractControl | null {
+  const controlParent = inject(token)
+  return controlParent ? controlParent.get(path) : null
+}
