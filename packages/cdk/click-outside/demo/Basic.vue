8c8becdd283c4a081ec7bbc02c02164773fc6b22
LaamGinghong <351390560@qq.com>
2021-02-16 22:58:23 +0800

    feat(cdk: click-outside): add cdk click-outside
    add cdk click-outside.

---
+++ packages/cdk/click-outside/demo/Basic.vue
@@ -0,0 +1,25 @@
+<template>
+  <ix-button @click.stop="toggle">toggle</ix-button>
+  <p v-if="visible" v-click-outside="toggle" @click="log">Hello world</p>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { clickOutside } from '@idux/cdk/click-outside'
+
+export default defineComponent({
+  directives: { clickOutside },
+  setup() {
+    const visible = ref(false)
+
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+
+    const log = () => {
+      console.log('hello world')
+    }
+
+    return { visible, toggle, log }
+  },
+})
+</script>
