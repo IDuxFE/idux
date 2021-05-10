b3c14fb31187c1e8933f28a001c9f97dd4ec5b81
LaamGinghong <351390560@qq.com>
2021-05-10 17:21:42 +0800

    refactor(cdk: overlay): export types

--- packages/cdk/overlay/index.ts
+++ packages/cdk/overlay/index.ts
@@ -1,1 +1,12 @@
 export * from './src/useOverlay'
+
+export type {
+  OverlayOptions,
+  OverlayInstance,
+  OverlayScrollStrategy,
+  OverlayPlacement,
+  OverlayTrigger,
+  OverlayElement,
+  OverlayTriggerEvents,
+  OverlayPopperEvents,
+} from './src/types'
