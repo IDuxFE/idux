1ba8d6c1a574443205929835f1668c58727a3f24
danranVm <danranvm@gmail.com>
2021-05-20 17:09:08 +0800

    feat(comp:button): add type props

--- packages/components/button/src/types.ts
+++ packages/components/button/src/types.ts
@@ -1,6 +1,8 @@
 import type { DefineComponent } from 'vue'
 import type { ButtonSize } from '@idux/components/config'
 
+import { PropTypes } from '@idux/cdk/utils'
+
 export type ButtonMode = 'primary' | 'default' | 'dashed' | 'text' | 'link'
 export type ButtonShape = 'circle' | 'round'
 
@@ -14,6 +16,20 @@ export interface ButtonProps {
   shape?: ButtonShape
   block?: boolean
   icon?: string
+  type: string
+}
+
+export const buttonPropsDef = {
+  mode: PropTypes.oneOf(['primary', 'default', 'dashed', 'text', 'link'] as const),
+  danger: PropTypes.bool,
+  ghost: PropTypes.bool,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  size: PropTypes.oneOf(['large', 'medium', 'small'] as const),
+  shape: PropTypes.oneOf(['circle', 'round'] as const),
+  block: PropTypes.bool,
+  icon: PropTypes.string,
+  type: PropTypes.string.def('button'),
 }
 
 export type ButtonInstance = InstanceType<DefineComponent<ButtonProps>>
@@ -22,6 +38,12 @@ export interface ButtonGroupProps {
   mode?: ButtonMode
   size?: ButtonSize
   shape?: ButtonShape
+}
+
+export const buttonGroupPropsDef = {
+  mode: PropTypes.oneOf(['primary', 'default', 'dashed', 'text', 'link'] as const),
+  size: PropTypes.oneOf(['large', 'medium', 'small'] as const),
+  shape: PropTypes.oneOf(['circle', 'round'] as const),
 }
 
 export type ButtonGroupInstance = InstanceType<DefineComponent<ButtonGroupProps>>
