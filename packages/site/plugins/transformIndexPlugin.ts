a6a27ae98b2283b52203e6167ef8728c413450c6
danranVm <danranvm@gmail.com>
2021-05-31 14:55:10 +0800

    build: add transform index html plugin (#336)

---
+++ packages/site/plugins/transformIndexPlugin.ts
@@ -0,0 +1,27 @@
+import type { Plugin } from 'vite'
+
+const serveHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@IDUX</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script type="module" src="/src/main.ts"></script>
+  </body>
+</html>
+`
+
+export function transformIndexPlugin(): Plugin {
+  return {
+    name: 'idux:transformIndex',
+    enforce: 'pre',
+    transformIndexHtml(html, ctx) {
+      const isServe = ctx.server ? ctx.server.config.command === 'serve' : false
+      return isServe ? serveHtml : html
+    },
+  }
+}
