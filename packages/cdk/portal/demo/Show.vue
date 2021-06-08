5b02743064fbdb72a697b0958d9efc89735220ef
danranVm <danranvm@gmail.com>
2021-06-08 16:00:45 +0800

    feat(cdk:portal): support show props (#344)

---
+++ packages/cdk/portal/demo/Show.vue
@@ -0,0 +1,16 @@
+<template>
+  <ix-portal target="ix-container" :show="show">
+    <div>Portal</div>
+  </ix-portal>
+  <ix-button @click="show = !show">Show</ix-button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup() {
+    const show = ref(false)
+    return { show }
+  },
+})
+</script>
