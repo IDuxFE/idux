673cd1fdef6f46d1e9893c9f013dbd4d92fb5e72
danranVm <danranvm@gmail.com>
2021-06-29 17:25:37 +0800

    feat(cdk:scroll): add scroll locker

---
+++ packages/cdk/scroll/index.ts
@@ -0,0 +1,2 @@
+export * from './src/scrollLocker'
+export * from './src/utils'
--- scripts/gulp/site/utils.ts
+++ scripts/gulp/site/utils.ts
@@ -30,7 +30,7 @@ export function initSite(): void {
   })
 
   const filterPackageName = ['site']
-  const filterComponentName = ['node_modules', 'style', 'i18n', 'version', 'utils']
+  const filterComponentName = ['node_modules', 'style', 'i18n', 'version', 'utils', 'scroll']
   readdirSync(packageRoot).forEach(packageName => {
     if (filterPackageName.includes(packageName)) {
       return
