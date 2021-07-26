de6fc52b17ee1f12e7db48ba0423e3b42d2c4580
danranVm <danranvm@gmail.com>
2021-07-26 14:19:33 +0800

    fix(comp:overlay): render test add slots

--- packages/components/_private/overlay/__tests__/overlay.spec.ts
+++ packages/components/_private/overlay/__tests__/overlay.spec.ts
@@ -1,35 +1,24 @@
-import type { MountingOptions, VueWrapper } from '@vue/test-utils'
+import type { MountingOptions } from '@vue/test-utils'
 import type { OverlayProps } from '../src/types'
 
+import { h } from 'vue'
 import { mount } from '@vue/test-utils'
 import { renderWork } from '@tests'
-import IxOverlay from '../src/Overlay'
+import Overlay from '../src/Overlay'
 
 describe('Overlay', () => {
-  const OverlayMount: (options?: MountingOptions<Partial<OverlayProps>>) => VueWrapper<InstanceType<typeof IxOverlay>> =
-    (options = {}) => mount(IxOverlay, { ...options })
+  const OverlayMount = (options?: MountingOptions<Partial<OverlayProps>>) => mount(Overlay, { ...options })
 
-  beforeEach(() => {
-    jest.spyOn(console, 'warn').mockImplementation(() => {})
+  renderWork(Overlay, {
+    slots: {
+      trigger: () => h('div', 'trigger'),
+      overlay: () => h('div', 'overlay'),
+    },
   })
 
-  renderWork(IxOverlay)
-
-  test('no slots', () => {
+  test('no slots work', () => {
     const error = jest.spyOn(console, 'error').mockImplementation(() => {})
     OverlayMount()
     expect(error).toBeCalled()
-  })
-
-  test('show arrow', async () => {
-    const overlayWrapper = OverlayMount({
-      slots: {
-        trigger: '',
-        overlay: '',
-      },
-      props: { showArrow: true, visible: true },
-    })
-    expect(overlayWrapper.vm.showArrow).toBeTruthy()
-    expect(document.querySelector('.ix-overlay')?.querySelector('.ix-overlay-arrow')).toBeDefined()
   })
 })
