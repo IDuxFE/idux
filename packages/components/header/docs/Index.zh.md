70a8f76baad295a2e5af6a4e25e823b1c436c715
danranVm <danranvm@gmail.com>
2022-01-25 15:18:04 +0800

    docs(comp:*): api review (#741)

--- packages/components/header/docs/Index.zh.md
+++ packages/components/header/docs/Index.zh.md
@@ -15,18 +15,13 @@ order: 0
 | 名称 | 说明 | 类型  | 默认值 | 全局配置 | 备注 |
 | --- | --- | --- | --- | --- | --- |
 | `avatar` | 自定义头像 | `string \| AvatarProps \| #avatar` | - | - | 传入 `string` 时，为头像的图标  |
+| `description` | 标题下方的说明文字 | `string \| #description` | - | - | - |
 | `disabled` | 是否禁用 | `boolean` | `false` | - | - |
 | `prefix` | 标题前缀图标 | `string \| VNode \| #prefix` | - | - | - |
-| `size` | 标题大小 | `xl \| lg \| md \| sm` | `md` | - | 分别对应 `h1 \| h2 \| h3 \| h4` |
+| `size` | 标题大小 | `xl \| lg \| md \| sm` | `md` | - | - |
 | `showBar` | 是否显示标题前的竖条 | `boolean` | `false` | - | - |
 | `subTitle` | 二级标题文字 | `string \| #subTitle` | - | - | - |
 | `suffix` | 标题后缀图标 | `string \| VNode \| #suffix` | - | - | 通常用于额外操作 |
 | `title` | 标题文字 | `string \| #default` | - | - | - |
 | `onPrefixClick` | 前缀图标被点击 | `(evt: MouseEvent) => void` | - | - | - |
 | `onSuffixClick` | 后缀图标被点击 | `(evt: MouseEvent) => void` | - | - | - |
-
-#### HeaderSlots
-
-| 名称 | 说明 | 参数类型 | 备注 |
-| --- | --- | --- | --- |
-| `description` | 标题下方的说明文字 | - | - |
--- packages/components/header/src/Header.tsx
+++ packages/components/header/src/Header.tsx
@@ -5,18 +5,16 @@
  * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
  */
 
-import type { HeaderProps } from './types'
 import type { AvatarProps } from '@idux/components/avatar'
-import type { ComputedRef, Slot, VNode } from 'vue'
 
-import { computed, defineComponent, isVNode } from 'vue'
+import { type ComputedRef, type Slot, computed, defineComponent, normalizeClass } from 'vue'
 
 import { isString } from 'lodash-es'
 
 import { callEmit } from '@idux/cdk/utils'
 import { IxAvatar } from '@idux/components/avatar'
 import { useGlobalConfig } from '@idux/components/config'
-import { IxIcon } from '@idux/components/icon'
+import { covertIconVNode, covertLabelVNode } from '@idux/components/utils'
 
 import { headerProps } from './types'
 
@@ -34,7 +32,15 @@ export default defineComponent({
     const common = useGlobalConfig('common')
     const mergedPrefixCls = computed(() => `${common.prefixCls}-header`)
 
-    const classes = useClasses(props, mergedPrefixCls)
+    const classes = computed(() => {
+      const prefixCls = mergedPrefixCls.value
+      return normalizeClass({
+        [prefixCls]: true,
+        [`${prefixCls}-disabled`]: props.disabled,
+        [`${prefixCls}-with-bar`]: props.showBar,
+        [`${prefixCls}-${props.size}`]: true,
+      })
+    })
 
     const avatarSize = computed(() => avatarSizeTransformMap[props.size])
 
@@ -42,59 +48,37 @@ export default defineComponent({
     const onSuffixClick = (evt: MouseEvent) => !props.disabled && callEmit(props.onSuffixClick, evt)
 
     return () => {
-      const { prefix, suffix, avatar, title, subTitle } = props
       const prefixCls = mergedPrefixCls.value
+
+      const prefixIconNode = covertIconVNode(slots, props, 'prefix')
+      const suffixIconNode = covertIconVNode(slots, props, 'suffix')
+      const titleNode = covertLabelVNode(slots.default, props.title)
+      const subTitleNode = covertLabelVNode(slots, props, 'subTitle')
+      const descriptionNode = covertLabelVNode(slots, props, 'description')
+
       return (
         <div class={classes.value}>
           <div class={`${prefixCls}-content`}>
-            {renderIcon(slots.prefix, prefix, onPrefixClick, `${prefixCls}-prefix`)}
-            {renderAvatar(slots.avatar, avatar, avatarSize)}
-            {renderTitle(slots.default, title, `${prefixCls}-title`)}
-            {renderTitle(slots.subTitle, subTitle, `${prefixCls}-sub-title`)}
-            {renderIcon(slots.suffix, suffix, onSuffixClick, `${prefixCls}-suffix`)}
+            {prefixIconNode && (
+              <span class={`${prefixCls}-prefix`} onClick={onPrefixClick}>
+                {prefixIconNode}
+              </span>
+            )}
+            {renderAvatar(slots.avatar, props.avatar, avatarSize)}
+            {titleNode && <span class={`${prefixCls}-title`}>{titleNode}</span>}
+            {subTitleNode && <span class={`${prefixCls}-sub-title`}>{subTitleNode}</span>}
+            {suffixIconNode && (
+              <span class={`${prefixCls}-suffix`} onClick={onSuffixClick}>
+                {suffixIconNode}
+              </span>
+            )}
           </div>
-          {slots.description ? <div class={`${prefixCls}-description`}>{slots.description()}</div> : null}
+          {descriptionNode && <div class={`${prefixCls}-description`}>{descriptionNode}</div>}
         </div>
       )
     }
   },
 })
-
-const useClasses = (props: HeaderProps, mergedPrefixCls: ComputedRef<string>) => {
-  return computed(() => {
-    const prefixCls = mergedPrefixCls.value
-    return {
-      [prefixCls]: true,
-      [`${prefixCls}-bar`]: props.showBar,
-      [`${prefixCls}-disabled`]: props.disabled,
-      [`${prefixCls}-${props.size}`]: true,
-    }
-  })
-}
-
-const renderIcon = (
-  slot: Slot | undefined,
-  icon: string | VNode | undefined,
-  onClick: (evt: MouseEvent) => void,
-  wrapperClassName: string,
-) => {
-  if (!slot && !icon) {
-    return null
-  }
-  const iconNode = slot ? slot() : isVNode(icon) ? icon : <IxIcon name={icon} />
-  return (
-    <div class={wrapperClassName} onClick={onClick}>
-      {iconNode}
-    </div>
-  )
-}
-
-const renderTitle = (slot: Slot | undefined, title: string | undefined, wrapperClassName: string) => {
-  if (!slot && !title) {
-    return null
-  }
-  return <span class={wrapperClassName}>{slot?.() ?? title}</span>
-}
 
 const renderAvatar = (
   slot: Slot | undefined,
