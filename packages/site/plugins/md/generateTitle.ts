11974d2c54a4a3cadbff391b655a56f3b7b6859a
Hawtim <hawtimzhang@gmail.com>
2022-03-21 09:14:55 +0800

    docs(comp:*): fix github edit page not found issue (#810)
    docs(comp:*): fix type error and typo

--- packages/site/plugins/md/generateTitle.ts
+++ packages/site/plugins/md/generateTitle.ts
@@ -1,15 +1,20 @@
-interface TitleMeta {
+export interface TitleMeta {
   title: string
   subtitle?: string
   path: string
+  type?: string | null
+  order?: number
+  single?: boolean
+  category: 'components' | 'docs' | 'pro' | 'cdk'
 }
 
 export function generateTitle(meta: TitleMeta): string {
+  const href = 'https://github.com/IDuxFE/idux/edit/main/'
   return `<h1>${meta.title}<span class="subtitle">${meta.subtitle || ''}</span>
   <a
     class="edit-button"
     aria-label="Edit this page on GitHub"
-    href="https://github.com/IDuxFE/idux/edit/main/${meta.path}"
+    href="${href}${meta.path}"
     target="_blank"
     rel="noopener noreferrer"
   >
