adda15d0ee85bafb0c4785a169e7d7e9d4617d41
danranvm <danranvm@gmail.com>
2022-04-26 16:31:41 +0800

    fix(comp: message): align icon with text

--- packages/components/message/demo/Content.vue
+++ packages/components/message/demo/Content.vue
@@ -5,16 +5,14 @@
 <script lang="ts">
 import { defineComponent, h } from 'vue'
 
+import { IxIcon } from '@idux/components/icon'
 import { useMessage } from '@idux/components/message'
 
 export default defineComponent({
   setup() {
     const message = useMessage()
 
-    const content = h('ul', {}, [
-      h('li', {}, 'This is a VNode content data'),
-      h('li', {}, 'This is a VNode content data'),
-    ])
+    const content = h('span', {}, [h('span', {}, 'This is a VNode content '), h(IxIcon, { name: 'star' })])
 
     const open = () => message.info(content)
 
