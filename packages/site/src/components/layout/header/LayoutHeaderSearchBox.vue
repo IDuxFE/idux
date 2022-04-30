e4187162d8b271aafc822d2f90f5c52fcdb4c99f
X.Q. Chen <31237954+brenner8023@users.noreply.github.com>
2022-04-30 20:44:21 +0800

    feat: add site searcher

--- packages/site/src/components/layout/header/LayoutHeaderSearchBox.vue
+++ packages/site/src/components/layout/header/LayoutHeaderSearchBox.vue
@@ -1,22 +1,60 @@
 <template>
   <div class="header-search-box">
-    <IxInput borderless prefix="search" :placeholder="placeholder" disabled />
+    <IxSelect
+      virtual
+      borderless
+      searchable
+      suffix="search"
+      style="width: 180px"
+      :dataSource="allOptions"
+      :placeholder="placeholder"
+      :searchFn="filterSearch"
+      @change="onSelectChange"
+    ></IxSelect>
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, inject } from 'vue'
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+
+import { useRouter } from 'vue-router'
 
 import { appContextToken } from '../../../context'
+import { config } from '../../../sideNav'
 
-export default defineComponent({
-  setup() {
-    const { lang } = inject(appContextToken)!
-    const placeholder = computed(() => {
-      return lang.value == 'zh' ? '在 idux.site 中搜索' : 'Search in idux.site'
+const router = useRouter()
+const { lang } = inject(appContextToken)!
+const placeholder = computed(() => {
+  return lang.value == 'zh' ? '在 idux.site 中搜索' : 'Search in idux.site'
+})
+
+const filterSearch = (optionData, searchVal: string) => {
+  return optionData.label.toLowerCase().includes(searchVal.toLowerCase())
+}
+
+const onSelectChange = (newVal: string) => {
+  newVal && router.push(newVal)
+}
+
+const validConfig = config => config.path && config.title
+
+const getLabel = config => [config.title, config.subtitle].join(' ').trim()
+
+const allOptions = computed(() => {
+  const optionMap: Record<string, string> = {}
+  Object.values(config)
+    .reduce((total, currentList) => total.concat(currentList.filter(curr => curr.lang === lang.value)), [])
+    .forEach(item => {
+      ;(item.children ?? []).forEach(child => {
+        validConfig(child) && (optionMap[child.path] = getLabel(child))
+      })
+      validConfig(item) && (optionMap[item.path] = getLabel(item))
     })
 
-    return { placeholder }
-  },
+  return Object.entries(optionMap).map(([k, v]) => ({
+    key: k,
+    label: v,
+    value: k,
+  }))
 })
 </script>
