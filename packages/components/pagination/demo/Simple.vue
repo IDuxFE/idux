06535c174a5ec32ff1d3c47fcb4fbb74b0610d04
liuzaijiang <530604689@qq.com>
2022-05-25 10:26:13 +0800

    fix(comp: pagination): Showquickjumper does not work in simple mode

--- packages/components/pagination/demo/Simple.vue
+++ packages/components/pagination/demo/Simple.vue
@@ -1,5 +1,7 @@
 <template>
-  <IxPagination :total="500" simple />
-  <br />
-  <IxPagination :total="500" simple disabled />
+  <IxSpace vertical block>
+    <IxPagination :total="500" simple />
+    <IxPagination :total="500" simple showQuickJumper />
+    <IxPagination :total="500" simple disabled />
+  </IxSpace>
 </template>
