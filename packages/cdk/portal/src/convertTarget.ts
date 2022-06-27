75c24b55327aae9b2b96a880f1f515f10c8baf29
X.Q. Chen <31237954+brenner8023@users.noreply.github.com>
2022-06-28 00:16:14 +0800

    fix(cdk:portal): remove targetHashMap

--- packages/cdk/portal/src/convertTarget.ts
+++ packages/cdk/portal/src/convertTarget.ts
@@ -9,7 +9,10 @@ import type { PortalTargetType } from './types'
 
 import { isFunction, isString } from 'lodash-es'
 
-const targetHashmap: Record<string, HTMLElement> = {}
+function tryFindElement(selector: string) {
+  const classSelector = `.${selector}`
+  return document.querySelector<HTMLElement>(selector) ?? document.querySelector<HTMLElement>(classSelector)
+}
 
 export function convertTarget(target: PortalTargetType): HTMLElement {
   const temp = isFunction(target) ? target() : target
@@ -18,18 +21,12 @@ export function convertTarget(target: PortalTargetType): HTMLElement {
     return temp
   }
 
-  if (targetHashmap[temp]) {
-    return targetHashmap[temp]
-  }
-
-  let element = document.querySelector<HTMLElement>(temp)
+  let element = tryFindElement(temp)
   if (!element) {
     element = document.createElement('div')
     element.classList.add(temp)
     document.body.appendChild(element)
   }
-
-  targetHashmap[temp] = element
 
   return element
 }
