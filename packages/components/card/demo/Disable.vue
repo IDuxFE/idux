a2634970ea8738fc736be99673b0a7432ec217e3
lionnnnn <601035219@qq.com>
2022-06-28 11:56:21 +0800

    feat(comp:card): add selection state and disabled state (#983)
    fix #972

---
+++ packages/components/card/demo/Disable.vue
@@ -0,0 +1,11 @@
+<template>
+  <IxRow>
+    <IxCol xs="24" sm="8">
+      <IxCard :header="{ title: 'Card title', extra: 'setting' }" disabled>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </IxCard>
+    </IxCol>
+  </IxRow>
+</template>
---
+++ packages/components/card/demo/Selectable.vue
@@ -0,0 +1,55 @@
+<template>
+  <IxRow>
+    <IxCol xs="24" sm="8">
+      <div>Selected: {{ selected1 }}</div>
+      <IxCard
+        v-model:selected="selected1"
+        :header="{ title: 'Selectable', extra: 'setting' }"
+        size="lg"
+        selectable
+        @selectedChange="logSelected"
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </IxCard>
+    </IxCol>
+    <IxCol xs="24" sm="8">
+      <div>Selected: {{ selected2 }}</div>
+      <IxCard
+        v-model:selected="selected1"
+        :header="{ title: 'Disabled and selected', extra: 'setting' }"
+        selectable
+        disabled
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </IxCard>
+    </IxCol>
+    <IxCol xs="24" sm="8">
+      <div>Selected: {{ selected3 }}</div>
+      <IxCard
+        v-model:selected="selected1"
+        :header="{ title: 'Disable and unselected', extra: 'setting' }"
+        selectable
+        disabled
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </IxCard>
+    </IxCol>
+  </IxRow>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const selected1 = ref(true)
+const selected2 = ref(true)
+const selected3 = ref(false)
+
+const logSelected = selected => {
+  console.log(`selected is ${selected}`)
+}
+</script>
