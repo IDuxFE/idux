49863e9d8943ef7ec1bd98bf5bffe6c4e8418419
tuchg <24775733+tuchg@users.noreply.github.com>
2022-10-20 11:55:04 +0800

    fix(comp:watermark): flickering problem (#1206)

--- packages/components/watermark/src/composables/useAntiTamper.ts
+++ packages/components/watermark/src/composables/useAntiTamper.ts
@@ -5,11 +5,9 @@
  * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
  */
 
-import { watch } from 'vue'
+import { NoopFunction } from '@idux/cdk/utils'
 
-import { MaybeElementRef, convertElement } from '@idux/cdk/utils'
-
-export function useAntiTamper(wrapperRef: MaybeElementRef, elRef: MaybeElementRef): void {
+export function useAntiTamper(wrapper: HTMLElement, el: HTMLElement): { stop: typeof NoopFunction } {
   const options: MutationObserverInit = {
     childList: true,
     subtree: true,
@@ -17,49 +15,36 @@ export function useAntiTamper(wrapperRef: MaybeElementRef, elRef: MaybeElementRe
     attributeOldValue: true,
   }
 
-  let observer: MutationObserver | null = null
-  let el: HTMLElement | null | undefined = null
-
-  watch(
-    () => convertElement(wrapperRef)!,
-    wrapper => {
-      if (!el) {
-        el = convertElement(elRef)
-      }
-
-      if (wrapper && !observer) {
-        observer = new MutationObserver((records: MutationRecord[]) => {
-          records
-            .filter(record => record.target === el || record.target === wrapper)
-            .forEach(record => {
-              // avoid modifying data
-              if (record.type === 'attributes') {
-                reset(() => {
-                  el!.setAttribute(record.attributeName!, record.oldValue!)
-                })
-              }
-              // avoid deleted
-              if (record.type === 'childList') {
-                reset(() => {
-                  wrapper!.appendChild(record.removedNodes[0])
-                })
-              }
-            })
-        })
-
-        observer.observe(wrapper, options)
-
-        const reset = (resume = () => {}) => {
-          setTimeout(() => {
-            observer?.disconnect()
-            resume()
-            observer?.observe(wrapper, options)
-          }, 0)
+  const observer = new MutationObserver((records: MutationRecord[]) => {
+    records
+      .filter(record => record.target === el || record.target === wrapper)
+      .forEach(record => {
+        // avoid modifying data
+        if (record.type === 'attributes') {
+          reset(() => {
+            el!.setAttribute(record.attributeName!, record.oldValue!)
+          })
         }
-      }
-    },
-    {
-      immediate: true,
-    },
-  )
+        // avoid deleted
+        if (record.type === 'childList') {
+          reset(() => {
+            wrapper!.appendChild(record.removedNodes[0])
+          })
+        }
+      })
+  })
+
+  observer.observe(wrapper, options)
+
+  const reset = (resume = NoopFunction) => {
+    requestAnimationFrame(() => {
+      observer.disconnect()
+      resume()
+      observer.observe(wrapper, options)
+    })
+  }
+
+  return {
+    stop: () => observer.disconnect(),
+  }
 }
