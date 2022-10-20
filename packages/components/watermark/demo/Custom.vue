1ef05b2c7770d46c796dc3e8a8b2c622914d4347
tuchg <24775733+tuchg@users.noreply.github.com>
2022-10-20 11:55:04 +0800

    fix(comp:watermark): flickering problem (#1206)

--- packages/components/watermark/demo/Custom.vue
+++ packages/components/watermark/demo/Custom.vue
@@ -2,18 +2,24 @@
   <IxWatermark
     :rotate="30"
     :opacity="0.2"
-    content="你好 iDux🚀"
+    :content="text"
     font="small-caps bold 24px HGKY_CNKI"
     fontColor="#1c6eff"
     :fontSize="24"
     :offsetLeft="10"
     :renderFn="canvasDebugger"
   >
-    <div style="height: 500px"></div>
+    <div style="height: 500px">
+      <IxInput v-model:value="text"></IxInput>
+    </div>
   </IxWatermark>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('你好 iDux🚀')
+
 const canvasDebugger = (ctx: CanvasRenderingContext2D) => {
   console.log(ctx.lineWidth)
 }
