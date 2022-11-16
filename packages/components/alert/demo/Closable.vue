628d02f575cc08360c49c0a4a71b495c047de06a
danranvm <danranvm@gmail.com>
2022-11-16 11:47:57 +0800

    fix(comp:alert): style update and css variable support

--- packages/components/alert/demo/Closable.vue
+++ packages/components/alert/demo/Closable.vue
@@ -1,9 +1,11 @@
 <template>
-  <IxAlert closable @beforeClose="onBeforeClose" @close="onClose"> 雪崩的时候，没有一片雪花是不崩的。</IxAlert>
-  <IxAlert closable type="error" @beforeClose="onBeforeClose" @close="onClose"> 香蕉越大，香蕉皮越大 </IxAlert>
-  <IxAlert closable type="warning" @beforeClose="onBeforeClose" @close="onClose">
-    如果你愿意多花一点时间读完，你就会发现你多花了一点时间。
-  </IxAlert>
+  <IxSpace block vertical>
+    <IxAlert closable @beforeClose="onBeforeClose" @close="onClose"> 雪崩的时候，没有一片雪花是不崩的。</IxAlert>
+    <IxAlert closable type="error" @beforeClose="onBeforeClose" @close="onClose"> 香蕉越大，香蕉皮越大 </IxAlert>
+    <IxAlert closable type="warning" @beforeClose="onBeforeClose" @close="onClose">
+      如果你愿意多花一点时间读完，你就会发现你多花了一点时间。
+    </IxAlert>
+  </IxSpace>
 </template>
 
 <script lang="ts" setup>
