04efc147c01c35e9875845d6c944bee0e257eadf
danranvm <danranvm@gmail.com>
2022-12-26 19:59:54 +0800

    fix(comp:alert): style update

---
+++ packages/components/alert/demo/LongString.vue
@@ -0,0 +1,13 @@
+<template>
+  <IxAlert>
+    <Vue3Marquee>
+      我长话短说，但是说来话长。我长话短说，但是说来话长。我长话短说，但是说来话长。我长话短说，但是说来话长。
+    </Vue3Marquee>
+  </IxAlert>
+</template>
+
+<script setup lang="ts">
+// eslint-disable-next-line import/no-unassigned-import
+import 'vue3-marquee/dist/style.css'
+import { Vue3Marquee } from 'vue3-marquee'
+</script>
