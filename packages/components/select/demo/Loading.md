77cce6ccccfb3f39e5759afe5f495b5c6ffa7cc5
Konv Suu <2583695112@qq.com>
2023-01-26 01:22:09 +0800

    feat(comp:select): add loading prop

---
+++ packages/components/select/demo/Loading.md
@@ -0,0 +1,13 @@
+---
+title:
+  zh: 加载中
+  en: Loading
+order: 51
+---
+## zh
+
+数据还未加载完成时，展示一个加载中的状态。
+
+## en
+
+When the data has not been loaded yet, display a loading status。
---
+++ packages/components/select/demo/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <IxSpace vertical>
+    <IxSelect v-model:value="value" :dataSource="dataSource" :spin="loading"></IxSelect>
+    <IxButton @click="onLoad">Load Data</IxButton>
+  </IxSpace>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { SelectData } from '@idux/components/select'
+
+const dataSource = ref<SelectData[]>([{ label: 'Tom', value: 'tom' }])
+const value = ref('tom')
+const loading = ref(false)
+
+const onLoad = () => {
+  dataSource.value = [{ label: 'Tom', value: 'tom' }]
+  value.value = 'tom'
+
+  if (loading.value) {
+    return
+  }
+
+  loading.value = true
+  setTimeout(() => {
+    loading.value = false
+    dataSource.value = [
+      { label: 'Tom', value: 'tom' },
+      { label: 'Jack', value: 'jack' },
+      { label: 'Lucy', value: 'lucy' },
+    ]
+  }, 3000)
+}
+</script>
