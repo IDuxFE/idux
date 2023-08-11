0cd0c34aaa1124d115ba9911e2ae8ba5e9c46947
saller <weskerli@163.com>
2023-08-11 18:33:10 +0800

    docs(comp:modal): update typed modal demo (#1639)

--- packages/components/modal/demo/Type.vue
+++ packages/components/modal/demo/Type.vue
@@ -1,4 +1,15 @@
 <template>
+  <h3>Without Header</h3>
+  <IxSpace>
+    <IxButton @click="confirmNoHeaderVisible = !confirmNoHeaderVisible">Confirm</IxButton>
+    <IxButton @click="infoNoHeaderVisible = !infoNoHeaderVisible">Info</IxButton>
+    <IxButton @click="successNoHeaderVisible = !successNoHeaderVisible">Success</IxButton>
+    <IxButton @click="warningNoHeaderVisible = !warningNoHeaderVisible">Warning</IxButton>
+    <IxButton @click="errorNoHeaderVisible = !errorNoHeaderVisible">Error</IxButton>
+  </IxSpace>
+  <br />
+  <br />
+  <h3>With Header</h3>
   <IxSpace>
     <IxButton @click="confirmVisible = !confirmVisible">Confirm</IxButton>
     <IxButton @click="infoVisible = !infoVisible">Info</IxButton>
@@ -6,19 +17,35 @@
     <IxButton @click="warningVisible = !warningVisible">Warning</IxButton>
     <IxButton @click="errorVisible = !errorVisible">Error</IxButton>
   </IxSpace>
-  <IxModal v-model:visible="confirmVisible" type="confirm" title="Are you sure delete this task?">
+  <IxModal v-model:visible="confirmVisible" header="Title" type="confirm" title="Are you sure delete this task?">
     <p>Some descriptions...</p>
   </IxModal>
-  <IxModal v-model:visible="infoVisible" type="info" title="This is a notification message">
+  <IxModal v-model:visible="infoVisible" header="Title" type="info" title="This is a notification message">
     <p>Some messages...</p>
   </IxModal>
-  <IxModal v-model:visible="successVisible" type="success" title="This is a success message">
+  <IxModal v-model:visible="successVisible" header="Title" type="success" title="This is a success message">
     <p>Some messages...</p>
   </IxModal>
-  <IxModal v-model:visible="warningVisible" type="warning" title="This is an warning message">
+  <IxModal v-model:visible="warningVisible" header="Title" type="warning" title="This is an warning message">
     <p>Some messages...</p>
   </IxModal>
-  <IxModal v-model:visible="errorVisible" type="error" title="This is an error message">
+  <IxModal v-model:visible="errorVisible" header="Title" type="error" title="This is an error message">
+    <p>Some messages...</p>
+  </IxModal>
+
+  <IxModal v-model:visible="confirmNoHeaderVisible" type="confirm" title="Are you sure delete this task?">
+    <p>Some descriptions...</p>
+  </IxModal>
+  <IxModal v-model:visible="infoNoHeaderVisible" type="info" title="This is a notification message">
+    <p>Some messages...</p>
+  </IxModal>
+  <IxModal v-model:visible="successNoHeaderVisible" type="success" title="This is a success message">
+    <p>Some messages...</p>
+  </IxModal>
+  <IxModal v-model:visible="warningNoHeaderVisible" type="warning" title="This is an warning message">
+    <p>Some messages...</p>
+  </IxModal>
+  <IxModal v-model:visible="errorNoHeaderVisible" type="error" title="This is an error message">
     <p>Some messages...</p>
   </IxModal>
 </template>
@@ -31,4 +58,10 @@ const infoVisible = ref(false)
 const successVisible = ref(false)
 const warningVisible = ref(false)
 const errorVisible = ref(false)
+
+const confirmNoHeaderVisible = ref(false)
+const infoNoHeaderVisible = ref(false)
+const successNoHeaderVisible = ref(false)
+const warningNoHeaderVisible = ref(false)
+const errorNoHeaderVisible = ref(false)
 </script>
