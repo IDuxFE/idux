60143dd158547f6570fe6c4c098e68b4f1a77228
saller <weskerli@163.com>
2023-10-23 10:05:25 +0800

    style(comp:table): modify table expandable icon global config for seer (#1719)

--- packages/components/config/src/seerConfig.ts
+++ packages/components/config/src/seerConfig.ts
@@ -5,11 +5,18 @@
  * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
  */
 
-import { h } from 'vue'
+import { type VNodeChild, h } from 'vue'
 
 import { IxIcon } from '@idux/components/icon'
 
 import { type DeepPartialGlobalConfig } from './globalConfig'
+
+const expandIconRenderer = ({ expanded }: { expanded: boolean }): VNodeChild =>
+  h('svg', { viewBox: '0 0 16 16', width: '14px', height: '14px' }, [
+    h('rect', { x: 1, y: 1, width: 16, height: 16, rx: 1, fill: '#EDF1F7' }),
+    h('rect', { x: 6, y: 8, width: 6, height: 2, rx: 0.2, fill: '#5E6573' }),
+    !expanded ? h('rect', { x: 8, y: 6, width: 2, height: 6, rx: 0.2, fill: '#5E6573' }) : null,
+  ])
 
 export const seerConfig: DeepPartialGlobalConfig = {
   common: {
@@ -47,19 +54,14 @@ export const seerConfig: DeepPartialGlobalConfig = {
   },
   table: {
     columnExpandable: {
-      icon: ({ expanded }) => h(IxIcon, { name: expanded ? 'minus-square' : 'plus-square' }),
+      icon: expandIconRenderer,
     },
   },
   timePicker: {
     allowInput: true,
   },
   tree: {
-    expandIcon: ({ expanded }) =>
-      h('svg', { viewBox: '0 0 16 16', width: '14px', height: '14px' }, [
-        h('rect', { x: 1, y: 1, width: 16, height: 16, rx: 1, fill: '#EDF1F7' }),
-        h('rect', { x: 6, y: 8, width: 6, height: 2, rx: 0.2, fill: '#5E6573' }),
-        !expanded ? h('rect', { x: 8, y: 6, width: 2, height: 6, rx: 0.2, fill: '#5E6573' }) : null,
-      ]),
+    expandIcon: expandIconRenderer,
   },
   text: {
     copyIcon: ({ copied }) =>
