5676e0b02e2f0d01c2730286eba5ec4a34691bd6
sallerli1 <weskerli@163.com>
2023-11-23 16:02:43 +0800

    feat(cdk:theme): add cdk theme support

---
+++ packages/cdk/theme/demo/Basic.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Platform:</h2>
+    <p>Is Browser: {{ isBrowser }}</p>
+    <p>Is Blink: {{ isBlink }}</p>
+    <p>Is Webkit: {{ isWebKit }}</p>
+    <p>Is iOS: {{ isIOS }}</p>
+    <p>Is Android: {{ isAndroid }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { isAndroid, isBlink, isBrowser, isIOS, isWebKit } from '@idux/cdk/platform'
+
+export default defineComponent({
+  setup() {
+    return { isBrowser, isBlink, isWebKit, isIOS, isAndroid }
+  },
+})
+</script>
