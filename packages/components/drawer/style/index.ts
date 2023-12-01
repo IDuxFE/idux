0bd7cc9b796e4081bf08b0527af5259c46e83d98
saller <weskerli@163.com>
2023-12-01 23:12:48 +0800

    fix: style dependencies missing in on-demand import entries (#1755)

--- packages/components/drawer/style/index.ts
+++ packages/components/drawer/style/index.ts
@@ -1,5 +1,5 @@
 // style dependencies
-
+import '@idux/components/_private/mask/style'
 import '@idux/components/button/style'
 import '@idux/components/header/style'
 import '@idux/components/icon/style'
--- packages/pro/tree/style/index.ts
+++ packages/pro/tree/style/index.ts
@@ -3,5 +3,8 @@
 import '@idux/components/icon/style'
 import '@idux/components/button/style'
 import '@idux/components/divider/style'
+import '@idux/components/header/style'
+import '@idux/components/input/style'
+import '@idux/components/tree/style'
 
 import './index.less'
