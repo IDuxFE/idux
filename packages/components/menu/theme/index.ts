d31aa8124570d29a1fdcc1c9871cf7e5a12b1a54
sallerli1 <weskerli@163.com>
2023-12-11 19:25:19 +0800

    fix(pro:layout): dark theme overlay menu style error

--- packages/components/menu/theme/index.ts
+++ packages/components/menu/theme/index.ts
@@ -7,12 +7,11 @@
 
 import type { TokenGetter } from '@idux/components/theme'
 
+import { getDarkThemeTokens } from './dark'
 import { getDefaultThemeTokens } from './default'
 
 export const getThemeTokens: TokenGetter<'menu'> = (tokens, presetTheme, algorithms) => {
-  return presetTheme === 'default'
-    ? getDefaultThemeTokens(tokens, algorithms)
-    : getDefaultThemeTokens(tokens, algorithms)
+  return presetTheme === 'default' ? getDefaultThemeTokens(tokens, algorithms) : getDarkThemeTokens(tokens, algorithms)
 }
 
 export type { MenuThemeTokens } from './tokens'
