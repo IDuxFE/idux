a57373638f59ce4b77b1f7b58cb7fd8fd5fb930d
td <2019292338@qq.com>
2024-01-15 17:08:14 +0800

    fix(comp:empty): change empty slot demo style

--- packages/components/empty/demo/Content.vue
+++ packages/components/empty/demo/Content.vue
@@ -1,6 +1,12 @@
 <template>
   <IxEmpty>
-    <p> 关联影响和操作建议</p>
-    <IxButton mode="primary">引导按钮</IxButton>
+    <p class="label"> 关联影响和操作建议</p>
+    <IxButton size="lg" mode="primary">引导按钮</IxButton>
   </IxEmpty>
 </template>
+
+<style scoped>
+.label {
+  margin-bottom: 20px;
+}
+</style>
