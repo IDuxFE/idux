578508f1cc61cd42880df8f1585109fee554681e
sallerli1 <weskerli@163.com>
2024-04-24 18:36:19 +0800

    fix(comp:theme): store theme style reference count on style element

--- packages/components/theme/src/utils/dynamicCss.ts
+++ packages/components/theme/src/utils/dynamicCss.ts
@@ -10,6 +10,7 @@ import type { ContainerType, DynamicCssOptions } from '../types'
 import { isBrowser } from '@idux/cdk/platform'
 
 const IDUX_THEME_KEY = 'idux-theme-key'
+const IDUX_THEME_REF_CNT_KEY = '__IDUX_THEME_REF_CNT__'
 
 function findStyles(container: ContainerType): HTMLStyleElement[] {
   return Array.from(container.children).filter(node => node.tagName === 'STYLE') as HTMLStyleElement[]
@@ -24,10 +25,20 @@ function getContainer(option: DynamicCssOptions): ContainerType {
   return head || document.body
 }
 
-function findExistNode(key: string, option: DynamicCssOptions = {}): HTMLStyleElement | undefined {
+export function findExistNode(key: string, option: DynamicCssOptions = {}): HTMLStyleElement | undefined {
   const container = getContainer(option)
 
   return findStyles(container).find(node => node.getAttribute(IDUX_THEME_KEY) === key)
+}
+
+export function getStyleRefCnt(node: HTMLStyleElement): number {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return (node as any)[IDUX_THEME_REF_CNT_KEY] ?? 0
+}
+
+export function setStyleRefCnt(node: HTMLStyleElement, cnt: number): void {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ;(node as any)[IDUX_THEME_REF_CNT_KEY] = cnt
 }
 
 export function injectCSS(css: string, option: DynamicCssOptions = {}): HTMLStyleElement | null {
