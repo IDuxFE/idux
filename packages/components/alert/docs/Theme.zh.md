814f4a74617c19e117c9b07ac4205eb3ad520ea2
sallerli1 <weskerli@163.com>
2024-05-09 16:02:52 +0800

    feat(comp:alert): add theme tokens for typed alert text and bg colors 1. add typed alert theme tokens 2. change warining typed alert text and bg color to bronze and gold

--- packages/components/alert/docs/Theme.zh.md
+++ packages/components/alert/docs/Theme.zh.md
@@ -1,4 +1,14 @@
 | 名称 | 描述 | 类型 | default | dark |
 |---|---|---|---|---|
 | `borderRadius` | 边框圆角 | `number` | `2` | `2` |
+| `errorBgColor` | error 类型背景颜色 | `string` | `rgb(255, 238, 238)` | `rgb(54, 26, 27)` |
+| `errorTextColor` | error 类型文字颜色 | `string` | `#cf171d` | `#E8514C` |
 | `height` | 高度 | `number` | `32` | `32` |
+| `infoBgColor` | info 类型背景颜色 | `string` | `rgb(236, 244, 255)` | `rgb(21, 36, 58)` |
+| `infoTextColor` | info 类型文字颜色 | `string` | `#458fff` | `#4083E8` |
+| `offlineBgColor` | offline 类型背景颜色 | `string` | `rgb(246, 246, 247)` | `rgb(24, 27, 32)` |
+| `offlineTextColor` | offline 类型文字颜色 | `string` | `#6f7785` | `#808999` |
+| `successBgColor` | success 类型背景颜色 | `string` | `rgb(233, 250, 244)` | `rgb(21, 49, 42)` |
+| `successTextColor` | success 类型文字颜色 | `string` | `#12a679` | `#40C695` |
+| `warningBgColor` | warning 类型背景颜色 | `string` | `rgb(253, 247, 236)` | `rgb(54, 44, 25)` |
+| `warningTextColor` | warning 类型文字颜色 | `string` | `#a35f40` | `#D6A181` |
--- packages/components/alert/theme/default.ts
+++ packages/components/alert/theme/default.ts
@@ -5,28 +5,47 @@
  * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
  */
 
-import { type CertainThemeTokens, type GlobalThemeTokens, getAlphaColor } from '@idux/components/theme'
-export function getDefaultThemeTokens(tokens: GlobalThemeTokens): CertainThemeTokens<'alert'> {
+import {
+  type CertainThemeTokens,
+  type GlobalThemeTokens,
+  type ThemeTokenAlgorithms,
+  getAlphaColor,
+} from '@idux/components/theme'
+export function getDefaultThemeTokens(
+  tokens: GlobalThemeTokens,
+  algorithms: ThemeTokenAlgorithms,
+): CertainThemeTokens<'alert'> {
   const {
     heightMd,
     borderRadiusSm,
     alertCompColorAlpha,
     colorContainerBg,
+    colorSuccessText,
     colorSuccessBg,
+    colorInfoText,
     colorInfoBg,
-    colorWarningBg,
+    colorErrorText,
     colorErrorBg,
+    colorOfflineText,
     colorOffline,
   } = tokens
+  const { getColorPalette, getBaseColors } = algorithms
+
+  const bronzeColorPalette = getColorPalette(getBaseColors().bronze)
 
   return {
     height: heightMd,
     borderRadius: borderRadiusSm,
 
+    successTextColor: colorSuccessText,
     successBgColor: getAlphaColor(colorSuccessBg, alertCompColorAlpha, colorContainerBg),
+    infoTextColor: colorInfoText,
     infoBgColor: getAlphaColor(colorInfoBg, alertCompColorAlpha, colorContainerBg),
-    warningBgColor: getAlphaColor(colorWarningBg, alertCompColorAlpha, colorContainerBg),
+    warningTextColor: bronzeColorPalette.d10,
+    warningBgColor: getAlphaColor(getBaseColors().gold, alertCompColorAlpha, colorContainerBg),
+    errorTextColor: colorErrorText,
     errorBgColor: getAlphaColor(colorErrorBg, alertCompColorAlpha, colorContainerBg),
+    offlineTextColor: colorOfflineText,
     offlineBgColor: getAlphaColor(colorOffline, alertCompColorAlpha, colorContainerBg),
   }
 }
