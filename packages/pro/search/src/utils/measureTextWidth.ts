b520f2ef079622d30ca9c8a51640c2d5d7b0d198
sallerli1 <weskerli@163.com>
2024-09-27 15:09:06 +0800

    fix(pro:search): search item input text width measure incorrect

--- packages/pro/search/src/utils/measureTextWidth.ts
+++ packages/pro/search/src/utils/measureTextWidth.ts
@@ -8,11 +8,20 @@
 const measureElementStyle = {
   position: 'fixed',
   visibility: 'hidden',
-  whiteSpace: 'pre-wrap',
+  'white-space': 'pre-wrap',
   top: '-100px',
   left: '-100px',
 } as const
-const textWidthStyleProps = ['fontSize', 'letterSpacing'] as const
+const textWidthStyleProps = [
+  {
+    propertyKey: 'font-size',
+    computedStyleKey: 'fontSize',
+  },
+  {
+    propertyKey: 'letter-spacing',
+    computedStyleKey: 'letterSpacing',
+  },
+] as const
 
 export function measureTextWidth(text: string, parentEl?: HTMLElement): number {
   const _parentEl = parentEl as (HTMLElement & { __pro_search_measure_el: HTMLSpanElement }) | undefined
@@ -25,8 +34,8 @@ export function measureTextWidth(text: string, parentEl?: HTMLElement): number {
 
   if (parentEl) {
     const parentStyle = getComputedStyle(parentEl)
-    textWidthStyleProps.forEach(key => {
-      el.style.setProperty(key, parentStyle[key])
+    textWidthStyleProps.forEach(prop => {
+      el.style.setProperty(prop.propertyKey, parentStyle[prop.computedStyleKey])
     })
   }
 
